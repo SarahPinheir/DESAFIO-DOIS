1b899677bdfac6a00931ec40686737a7dcc8c612
Sarah <sarahleticiasr@gmail.com>
2021-07-30 23:40:38 -0300

    Desafio Dois Gama

---
+++ DotnetFrameworkDesafioDois/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DotnetFrameworkDesafioDois
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Bem-vindo(a) à Barraca da lentilha!"+"\n"+ "Digite em Kg a quantidade de lentilhas a serem compradas: ");
+            double preçoLentilha = 1.5;
+            double qtdLentilha = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("O valor a ser pago é "+ (preçoLentilha * qtdLentilha)+" real(is)");
+            Console.ReadKey();
+
+        }
+    }
+}
